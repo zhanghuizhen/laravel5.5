fcfaa3bd8156f3414cd0f307daf30de6ac4861e6
zhanghuizhen <1593131353@qq.com>
2019-04-01 21:19:25 +0800

    feat:创建评论和生活服务相关文件

---
+++ app/repositories/Comment.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pc
+ * Date: 2019/4/1
+ * Time: 21:18
+ */
+
+namespace App\repositories;
+
+
+class Comment
+{
+
+}
---
+++ app/repositories/Service.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pc
+ * Date: 2019/4/1
+ * Time: 21:16
+ */
+
+namespace App\repositories;
+
+
+class Service
+{
+
+}
